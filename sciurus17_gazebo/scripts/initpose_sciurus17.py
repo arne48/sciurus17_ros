48be80fbe14c00e51ffa949c697e17635fba51b2
Arne Hitzmann <arne.hitzmann@gmail.com>
2021-11-30 11:50:28 +0900

    added new maps and changed initial position for simulation

--- sciurus17_gazebo/scripts/initpose_sciurus17.py
+++ sciurus17_gazebo/scripts/initpose_sciurus17.py
@@ -21,8 +21,8 @@ pose_req.joint_names =     [ 'waist_yaw_joint', 'neck_yaw_joint', 'neck_pitch_jo
                              'r_arm_joint1', 'r_arm_joint2', 'r_arm_joint3', 'r_arm_joint4', 'r_arm_joint5', 'r_arm_joint6', 'r_arm_joint7', 'l_hand_joint', \
                              'l_arm_joint1', 'l_arm_joint2', 'l_arm_joint3', 'l_arm_joint4', 'l_arm_joint5', 'l_arm_joint6', 'l_arm_joint7', 'r_hand_joint' ]
 pose_req.joint_positions = [  0.0,  0.0,  0.0, \
-                                    0.0, -1.5,  0.0,  2.1,  0.0,  -2.0,  0.0,  0.0, \
-                                    0.0,  1.5,  0.0, -2.1,  0.0,   2.0,  0.0,  0.0  ]
+                                    -0.3574, -1.5707,  0.0,  2.7262,  0.0,  -1.1554,  0.0,  0.0, \
+                                    0.3574, 1.5707,  0.0,  -2.7262,  0.0,  1.1554,  0.0,  0.0]
 res = initialize_joints( pose_req )
 
 res = unpause_physics()
